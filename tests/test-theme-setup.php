0fb55a6190f0f6d7c7766f5832acfbd02445fa71
Nolan Young <nolanyoung7@yahoo.com>
2026-06-29 15:03:00 -0400

    test: fix lifecycle assertions

--- tests/test-theme-setup.php
+++ tests/test-theme-setup.php
@@ -8,7 +8,6 @@
 class NYTT01_Theme_Setup_Test extends WP_UnitTestCase {
 	/** @return void */
 	public function test_required_theme_supports_are_registered() {
-		do_action( 'after_setup_theme' );
 		$this->assertTrue( current_theme_supports( 'title-tag' ) );
 		$this->assertTrue( current_theme_supports( 'post-thumbnails' ) );
 		$this->assertTrue( current_theme_supports( 'html5' ) );
@@ -16,7 +15,6 @@ class NYTT01_Theme_Setup_Test extends WP_UnitTestCase {
 
 	/** @return void */
 	public function test_navigation_locations_are_registered() {
-		do_action( 'after_setup_theme' );
 		$locations = get_registered_nav_menus();
 		$this->assertArrayHasKey( 'primary', $locations );
 		$this->assertArrayHasKey( 'footer', $locations );
